645286d084d37db766732259b0056922aae23503
Jorge Santiago Ayala Sáenz <Jorge Santiago Ayala Senz>
2016-08-20 17:34:41 -0500

    c 1

--- MyApplication/app/src/test/java/com/example/kattyadesiderio/wilencoapp/ExampleUnitTest.java
+++ MyApplication/app/src/test/java/com/example/kattyadesiderio/wilencoapp/ExampleUnitTest.java
@@ -12,8 +12,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
     @Test
-    public void substraction_isCorrect() throws Exception {
-        assertEquals(0, 2 - 2);
+    public void multiplication_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
     }
 }
